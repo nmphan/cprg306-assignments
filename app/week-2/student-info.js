0940e7c78075efc8f966f415974440c5bec4d6c3
nmphan <mnpanthony@gmail.com>
2025-01-21 16:22:10 -0700

    week 2 implementation

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Nhat Minh Phan</h1>
+      <p>
+        <Link href="https://github.com/nmphan/cprg306-assignments">GitHub</Link>
+      </p>
+    </div>
+  );
+}
